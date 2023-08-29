b61efbce69b55231eea469c009bf70b3806120ed
Jorge Banegas <jorge.g.banegas@gmail.com>
2023-08-29 08:26:14 -0400

    chore: modificar urls

--- lib/api.dart
+++ lib/api.dart
@@ -1,5 +1,6 @@
 class Api {
-  static const String baseUrl = 'http://192.168.1.207:8001/api/v1';
-  static const String ulrAssistence = 'http://192.168.1.207:8002/api/v1';
-  static const String urlGeofencing = 'http://192.168.1.207:8004/api/v1';
+  static const String baseUrl =
+      'https://p01--auth-microservices--9wpsqj5d69c5.code.run/api/v1';
+  static const String ulrAssistence = 'http://104.45.156.208/api/v1';
+  static const String urlGeofencing = 'http://35.192.97.248/api/v1';
 }
